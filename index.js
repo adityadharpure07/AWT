4f199e8207a9cdc23c7f8d3ed8b481218845e336
adityadharpure07 <151540820+adityadharpure07@users.noreply.github.com>
2024-07-27 12:04:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log("hello world");
+// x = 10;
+// let x1 = 20;
+// var x2 = 30;
+// const x3 = 40;
+// let x4 = x1 + x2;
+// console.log(x4);
+
+// year = 2000
+// if(year % 100 == 0){
+//     if(year % 400 == 0){
+//         console.log("leap year..");
+//     }
+//     else console.log("not a Leap year....");
+// }
+// else{
+//     console.log("not a Leap year....");
+// }
+
+i=1
+while(i<=20){
+    console.log(i);
+    i++;
+}
+
+for(let i=0;i<=10;i++){
+    if(i%2 != 0){
+        console.log(i);
+    }
+}
+
---
+++ index1.js
@@ -0,0 +1,16 @@
+var fun = function(){
+    console.log("In Fun...");
+}
+
+fun();
+var add = function(a,b){
+    return a+b;
+}
+console.log(add(5,10))
+console.log(add("Hi"," There..."))
+console.log("1"+"30")
+
+var fun2 = () => {
+    console.log("text")
+}
+
