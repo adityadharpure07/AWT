d78eb6c7a6658410287b87796a0e6ed8ef3deabb
adityadharpure07 <151540820+adityadharpure07@users.noreply.github.com>
2024-08-03 12:27:14 +0530

    Add files via upload

---
+++ employ.js
@@ -0,0 +1,64 @@
+let employee = [
+    {
+    id : 5674,
+    fname :"emp1",
+    lname :"sur1",
+    salary :45000,
+    mobile :[9783437374],
+    projects:[{
+        id :123,
+        title : "bank_management",
+        skills : ["java","c++","js","frontend"]
+    }]
+    },
+    {
+    id : 5453,
+    fname :"emp2",
+    lname :"sur2",
+    salary :49000,
+    mobile :[9784888888],
+    projects:[{
+        id :34,
+        title : "bank_management",
+        skills : ["java","c++","js","frontend"]
+    }]
+    },
+    {
+    id : 5674,
+    fname :"emp1",
+    lname :"sur1",
+    salary :45000,
+    mobile :[9783437374],
+    projects:[{
+        id :123,
+        title : "bank_management",
+        skills : ["java","c++","js","frontend"]
+    }]
+    },
+    {
+    id : 5674,
+    fname :"emp1",
+    lname :"sur1",
+    salary :45000,
+    mobile :[9783437374],
+    projects:[{
+        id :123,
+        title : "bank_management",
+        skills : ["java","c++","js","frontend"]
+    },
+    {
+        id :223,
+        title : "library_management",
+        skills : ["java","c++","Angular","frontend"]
+    }
+]
+    }
+
+]
+// getpune = Students.filter(x => {
+//     if(x.city == "Pune") console.log(x.name)
+// })
+
+// allJava = employee.filter(x => {
+//     if(x.projects)
+// })
